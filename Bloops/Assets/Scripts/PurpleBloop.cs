e3cdff18e66ec89eba545275e4b451585f9e4566
Sheepie <chrisp@yesl.co.uk>
2020-02-26 09:38:51 +0000

    Neatening up files
    Neatening up some files and started setting up a new Bloop.

---
+++ Bloops/Assets/Scripts/PurpleBloop.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PurpleBloop : MonoBehaviour
+{
+	public float hunger = 0;
+	public float maxHunger = 100;
+
+	enum State
+	{
+		Thinking,
+		Moving,
+		Searching
+	}
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
